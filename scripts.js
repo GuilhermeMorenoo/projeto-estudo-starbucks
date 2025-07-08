3d327503ed1705b2ccd19ddbeb11053aa0b416fd
GuilhermeMorenoo <guimorenocosmo@outlook.com>
2025-07-07 23:18:11 -0300

    Primeiro Commit

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function trocarCor(cor) {
+    circulo.style.background = cor
+}
+
+let circulo = document.querySelector(".circulo")
+let imagem = document.querySelector(".starbucks")
+
+function trocaimagem(img) {
+    imagem.src = img
+}
